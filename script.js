bf4f0d62ea69d7706ac887d1aef53768e6f8d8a6
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-11 19:41:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let temp=str;
+	temp.sort();
+	for(int i=0;i<temp.length;i++){
+		if(i==0 && temp[i]!==temp[i+1] || i==temp.length-1 && temp[i]!==temp[i-1] || temp[i]!==temp[i-1] && temp[i]!==temp[i+1])return temp[i];
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
